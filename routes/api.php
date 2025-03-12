11d53f6e07537cf8aee517dc38785daae07b1863
sam002696 <sadmansakib221@gmail.com>
2025-03-12 17:30:16 +0600

    added authentication token message for invalid and missing tokens

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user']);
